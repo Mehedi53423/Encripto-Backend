cfa37346a5b5ee68b05645eb53b63b0f4be1847e
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-07-02 11:58:30 +0600

    Encoder & Decoder Added

---
+++ Encripto/Controllers/EncriptoController.cs
@@ -0,0 +1,27 @@
+﻿using Encripto.Response;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Encripto.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EncriptoController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Create(string inputString)
+        {
+            var result = EncoderAndDecoder.Encode(inputString);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get(string inputString)
+        {
+            var result = EncoderAndDecoder.Decode(inputString);
+            return Ok(result);
+        }
+    }
+}
---
+++ Encripto/EncoderAndDecoder.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Encripto
+{
+    public class EncoderAndDecoder
+    {
+        public static readonly string hash = "m€H€d!";
+
+        #region Encode
+        public static string Encode(string InputString)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(InputString);
+            byte[] keys = MD5.HashData(Encoding.UTF8.GetBytes(hash));
+            using TripleDESCryptoServiceProvider tripDes = new()
+            {
+                Key = keys,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform transform = tripDes.CreateEncryptor();
+            byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+            string output = Convert.ToBase64String(results, 0, results.Length);
+            return output;
+        }
+        #endregion
+
+        #region Decode
+        public static string Decode(string InputString)
+        {
+            byte[] data = Convert.FromBase64String(InputString);
+            byte[] keys = MD5.HashData(Encoding.UTF8.GetBytes(hash));
+            using TripleDESCryptoServiceProvider tripDes = new()
+            {
+                Key = keys,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform transform = tripDes.CreateDecryptor();
+            byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+            string output = Encoding.UTF8.GetString(results);
+            return output;
+        }
+        #endregion
+    }
+}
